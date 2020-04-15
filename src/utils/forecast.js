b69f6e5b043e6e5c2f77e6676a6ea88e1755f6eb
kal003 <ladkunal38@gmail.com>
2020-04-16 01:02:27 +0530

    Added high and low to forecast. Styles added too

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,13 +3,13 @@ const request = require('request');
 const forecast = (latitude, longitude, callback) => {
     const url = "https://api.openweathermap.org/data/2.5/weather?lat=" + encodeURIComponent(latitude)  + "&lon=" + encodeURIComponent(longitude) + "&APPID=717b0b9b7194a653ab1472f2796f7a78&units=metric"
 
-    request({url: url, json: true}, (error, response) => {
+    request({url: url, json: true}, (error, { body }) => {
         if(error) {
             callback('No Internet Connection', undefined)
-        } else if (response.body.message) {
+        } else if (body.message) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined , 'It is currently ' + response.body.main.temp + ' C',)
+            callback(undefined , 'It is currently ' + body.main.temp + ' C. The high today is ' + body.main.temp_max + ' C with a low of ' + body.main.temp_min + ' C')
         }
     })
 }
